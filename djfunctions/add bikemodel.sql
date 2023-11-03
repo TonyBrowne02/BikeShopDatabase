2237c866ccfdb6d28e548f0171596ae81ae00cc8
TonyBrowne02 <91893280+TonyBrowne02@users.noreply.github.com>
2023-11-03 14:37:14 +0000

    Update add bikemodel.sql
    DJ added trigger from this Tony Latop

--- djfunctions/add bikemodel.sql
+++ djfunctions/add bikemodel.sql
@@ -15,3 +15,19 @@ EXCEPTION
 END;
 $$ LANGUAGE plpgsql;
 GRANT EXECUTE ON FUNCTION bicycle_repair_shop.insert_new_modelbike(modelname VARCHAR, suppliers_supplierid INTEGER, quantityinstock INTEGER) TO owner;
+
+CREATE or replace FUNCTION audit_modelbike() RETURNS trigger AS $audit_bike$
+declare
+	uname varchar(80);
+	supplier_id varchar(80);
+	cdate date;
+BEGIN
+	select user into strict uname;
+	select now() into strict cdate;
+	insert into "Bike857A".model_bike_log values (new.modelid, new.modelname, new.suppliers_supplierid, new.quantityinstock, uname, cdate);
+	return new;
+END;
+$audit_bike$ LANGUAGE plpgsql;
+
+CREATE or replace TRIGGER audit_bike after INSERT OR UPDATE ON "Bike857A".modelbike
+FOR EACH ROW EXECUTE FUNCTION audit_bike();
