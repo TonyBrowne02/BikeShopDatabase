97017d82fc9e3be3ed21a1808beaf8610c6c7978
TonyBrowne02 <91893280+TonyBrowne02@users.noreply.github.com>
2023-11-03 15:18:47 +0000

    Separating addCustomer function and its trigger

--- TonyFunction/addCustomer.sql
+++ TonyFunction/addCustomer.sql
@@ -16,20 +16,4 @@ when others then
 END;
 $function$;
 
-CREATE or replace FUNCTION audit_customer() RETURNS trigger AS $audit_customer$
-declare
-	uname varchar(80);
-	cdate date;
-BEGIN
-	select user into strict uname;
-	select now() into strict cdate;
-	insert into "Bike857A".customer_log values (new.customerid, new.firstname, new.lastname, new.phonenumber, new.email, uname, cdate);
-	return new;
-END;
-$audit_customer$ LANGUAGE plpgsql;
-
-CREATE or replace TRIGGER audit_customer after INSERT OR UPDATE ON "Bike857A".customer
-FOR EACH ROW EXECUTE FUNCTION audit_customer();
-
-select addCustomer('Joey','Scmhoey','1234567890','joeySchmoey@email.com');
 
